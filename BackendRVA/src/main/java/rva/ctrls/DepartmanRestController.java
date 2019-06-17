5e552a4aeb46c0ad5399e786b7260b7dc5779fa3
milan-petrovic <milanpdo@gmail.com>
2019-06-17 09:22:08 +0200

    Import za crossorigin

--- BackendRVA/src/main/java/rva/ctrls/DepartmanRestController.java
+++ BackendRVA/src/main/java/rva/ctrls/DepartmanRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
